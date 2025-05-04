749ef92123b7d26ad04b9baf02b13ac7b6e4e6ce
dcejvan <dcejvan@uiowa.edu>
2025-05-04 17:24:33 -0500

    config change

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
+import rollupNodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
   base: './',
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyfills()],
+    },
+  },
 });
